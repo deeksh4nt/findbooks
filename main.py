433ce7751c6da519158fe11f9799114d06648d7f
Deekshant Yadav <deeksh4nt@gmail.com>
2024-01-04 18:11:57 +0530

    added recommendation server logic

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, Response, request
+from recommendation import recommend
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-  return 'Hello, World!'
+def root():
+  return "Provide ISBN value to get recommendations", 400
+
+@app.route('/<int:book>')
+def find_books(book):
+  try:
+    page = request.args.get('page', default=1, type=int)
+    books = recommend(book, page)
+    result = [str(b) for b in books]
+    return Response('\n'.join(result), mimetype='text/plain')
+  except:
+    return Response('invalid request', mimetype='text/plain')
