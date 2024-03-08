4c935651f9472e808274034d414d107a71eb0acd
Deekshant Yadav <142313612+deeksh4nt@users.noreply.github.com>
2024-03-08 14:52:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-  return "Provide ISBN value to get recommendations <br>Example <ul><li><a href=\"9780007136582\">/</a></li></ul>", 400
+  return "Provide ISBN value to get recommendations <br>Example <ul><li><a href=\"9780007136582\">/9780007136582 • The Lord of the Rings</a></li></ul>", 400
 
 @app.route('/<int:book>')
 def find_books(book):
